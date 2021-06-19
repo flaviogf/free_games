3d904899ef6337b7964489751ddb1d5d9eb3f381
flaviogf <flavio.fernandes6@gmail.com>
2021-06-19 17:42:49 -0300

    feat: adding game
    adding service

--- lib/free_games.rb
+++ lib/free_games.rb
@@ -2,8 +2,10 @@ require "json"
 require "securerandom"
 require "time"
 
+require "free_games/game"
 require "free_games/message"
 require "free_games/search"
+require "free_games/service"
 
 module FreeGames
 end
