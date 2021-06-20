2b3319690fe66417bf6ffe3356929c14c2a26335
flaviogf <flavio.fernandes6@gmail.com>
2021-06-20 10:17:34 -0300

    feat: including new files

--- lib/free_games.rb
+++ lib/free_games.rb
@@ -3,7 +3,9 @@ require "securerandom"
 require "time"
 
 require "free_games/game"
+require "free_games/game_adapter"
 require "free_games/message"
+require "free_games/messages"
 require "free_games/search"
 require "free_games/service"
 
