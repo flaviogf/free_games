a4fcba4f493b91ca7d93e5fb2b16b90409f271a1
flaviogf <flavio.fernandes6@gmail.com>
2021-06-20 10:16:05 -0300

    feat: ading method to_json

--- lib/free_games/message.rb
+++ lib/free_games/message.rb
@@ -1,4 +1,13 @@
 module FreeGames
   class Message < Struct.new(:uid, :updateDate, :titleText, :mainText, :redirectionUrl, keyword_init: true)
+    def to_json
+      {
+        uid: uid,
+        updateDate: updateDate.iso8601,
+        titleText: titleText,
+        mainText: mainText,
+        redirectionUrl: redirectionUrl,
+      }
+    end
   end
 end
--- spec/free_games/message_spec.rb
+++ spec/free_games/message_spec.rb
@@ -2,13 +2,11 @@ require "spec_helper"
 
 RSpec.describe FreeGames::Message do
   subject(:message) do
-    FreeGames::Message.new(
-      uid: SecureRandom.uuid,
-      updateDate: Time.now.utc.iso8601,
-      titleText: "Genshin Impact",
-      mainText: "Jogo Genshin Impact. Disponível entre os dias 12/06/2020 e 13/06/2020",
-      redirectionUrl: ""
-    )
+    FreeGames::Message.new uid: SecureRandom.uuid,
+                           updateDate: Time.parse("2020-06-12"),
+                           titleText: "Genshin Impact",
+                           mainText: "Jogo Genshin Impact. Disponível entre os dias 12/06/2020 e 13/06/2020",
+                           redirectionUrl: ""
   end
 
   it "should have property uid" do
@@ -29,5 +27,35 @@ RSpec.describe FreeGames::Message do
 
   it "should have property redirectionUrl" do
     expect(message.respond_to?(:redirectionUrl)).to be true
+  end
+
+  it "should have method to_json" do
+    expect(message.respond_to?(:to_json)).to be true
+  end
+
+  context "to_json" do
+    it "should return a Hash" do
+      expect(message.to_json).to be_a Hash
+    end
+
+    it "should have key uid" do
+      expect(message.to_json.has_key?(:uid)).to be true
+    end
+
+    it "should have key updateDate" do
+      expect(message.to_json.has_key?(:updateDate)).to be true
+    end
+
+    it "should have key titleText" do
+      expect(message.to_json.has_key?(:titleText)).to be true
+    end
+
+    it "should have key mainText" do
+      expect(message.to_json.has_key?(:mainText)).to be true
+    end
+
+    it "should have key redirectionUrl" do
+      expect(message.to_json.has_key?(:redirectionUrl)).to be true
+    end
   end
 end
