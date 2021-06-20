b44d2b5e8983611b7c37e4c884017076a10939ae
flaviogf <flavio.fernandes6@gmail.com>
2021-06-20 10:15:46 -0300

    feat: adding method to_json

--- lib/free_games/game.rb
+++ lib/free_games/game.rb
@@ -1,4 +1,12 @@
 module FreeGames
   class Game < Struct.new(:id, :title, :started_at, :ended_at, keyword_init: true)
+    def to_json
+      {
+        id: id,
+        title: title,
+        started_at: started_at.iso8601,
+        ended_at: ended_at.iso8601,
+      }
+    end
   end
 end
--- spec/free_games/game_spec.rb
+++ spec/free_games/game_spec.rb
@@ -1,11 +1,9 @@
 describe FreeGames::Game do
   subject(:game) do
-    FreeGames::Game.new(
-      id: SecureRandom.uuid,
-      title: "Genshin Impact",
-      started_at: Time.now.utc.iso8601,
-      ended_at: Time.now.utc.iso8601,
-    )
+    FreeGames::Game.new id: SecureRandom.uuid,
+                        title: "Genshin Impact",
+                        started_at: Time.parse("2020-06-12"),
+                        ended_at: Time.parse("2020-06-13")
   end
 
   it "should have property id" do
@@ -22,5 +20,31 @@ describe FreeGames::Game do
 
   it "should have property ended_at" do
     expect(game.respond_to?(:ended_at)).to be true
+  end
+
+  it "should have method to_json" do
+    expect(game.respond_to?(:to_json)).to be true
+  end
+
+  context "to_json" do
+    it "should return a hash" do
+      expect(game.to_json).to be_a Hash
+    end
+
+    it "should have key id" do
+      expect(game.to_json.has_key?(:id)).to be true
+    end
+
+    it "should have key title" do
+      expect(game.to_json.has_key?(:title)).to be true
+    end
+
+    it "should have key started_at" do
+      expect(game.to_json.has_key?(:started_at)).to be true
+    end
+
+    it "should have key ended_at" do
+      expect(game.to_json.has_key?(:ended_at)).to be true
+    end
   end
 end
