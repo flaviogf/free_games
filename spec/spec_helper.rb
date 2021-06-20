261d61d703924092ace76c64b1165c41ce3c2e20
flaviogf <flavio.fernandes6@gmail.com>
2021-06-20 10:16:26 -0300

    test: simplecov, rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
 require "functions_framework/testing"
 
 require "free_games"
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
